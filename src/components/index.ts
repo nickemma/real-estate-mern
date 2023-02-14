26eecd5d8e9c434a78ec7c08c554ec9860722dc3
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-02-14 16:49:48 +0100

    Created a reuseable card component

---
+++ src/components/index.ts
@@ -0,0 +1,32 @@
+// ======== Common ==========
+
+import Profile from './Common/Profile';
+import PropertyCard from './Common/PropertyCard';
+import customButton from './Common/CustomButton';
+import Form from './Common/Form';
+
+// ======== agent ==========
+
+import AgentCard from './agent/AgentCard';
+
+// ======== home ==========
+
+import TopAgent from './home/TopAgent';
+
+// ======== Charts ==========
+
+import PieChart from './charts/PieChart';
+import PropertyReferral from './charts/PropertyReferral';
+import TotalRevenue from './charts/TotalRevenue';
+
+export {
+  TotalRevenue,
+  PropertyReferral,
+  PieChart,
+  TopAgent,
+  AgentCard,
+  customButton,
+  Form,
+  Profile,
+  PropertyCard,
+};
